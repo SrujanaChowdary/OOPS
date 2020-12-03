4b8996fee9a37c227010bbceaccd1d9a29f61bfd
SrujanaChowdary <65702274+SrujanaChowdary@users.noreply.github.com>
2020-12-03 07:08:31 +0530

    Update and rename 50 to 50.cpp

--- 50.cpp
+++ 50.cpp
@@ -10,12 +10,12 @@ int main(){
   fstream file;
   file.open ("example.txt", ios::out | ios::in );
 
-  cout << "Write text to be written on file." << endl;
+  cout << "Write text to be written on file:";
   cin.getline(text, sizeof(text));
 
   // Writing on file
   file << text << endl;
-
+ cout<<"file content:";
   // Reding from file
   file >> text;
   cout << text << endl;
